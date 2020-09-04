d9d2c4a415f6988eb79988919024847f5294e8ed
Phillip Vendel Glimø <phillip.glimoe@hotmail.com>
2020-09-04 11:24:33 +0200

    .

--- src/gitTest/GroceryList.java
+++ src/gitTest/GroceryList.java
@@ -1,7 +1,27 @@
 package gitTest;
 
+import java.util.Arrays;
+
 public class GroceryList {
 
-    String[] GroceryItems = new String[10];
+    String[] listOfgroceryItems = new String[10];
+    private int listNumber = 0;
 
+
+    @Override
+    public String toString() {
+        return "GroceryList{" +
+                "listOfgroceryItems=" + Arrays.toString(listOfgroceryItems) +
+                '}';
+    }
+
+    public void addItemToGroceryItems(String item){
+        if (listOfgroceryItems[9] == null){
+            listOfgroceryItems[listNumber] = item;
+            listNumber++;
+        } else {
+            System.out.println("Du har allerede 10 ting på listen");
+        }
+
+    }
 }
--- src/gitTest/Main.java
+++ src/gitTest/Main.java
@@ -1,10 +1,14 @@
 package gitTest;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello Wrld");
-        System.out.println("kasper");
+        GroceryList groceryList = new GroceryList();
+
+
+        System.out.println(Arrays.toString(groceryList.listOfgroceryItems));
 
     }
 }
