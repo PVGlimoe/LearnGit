eb9abc4a364b0fd275469507a88a3d6337318943
kasper <kasperkbf@hotmail.com>
2020-09-04 10:33:09 +0200

    o

--- src/gitTest/Main.java
+++ src/gitTest/Main.java
@@ -3,7 +3,7 @@ package gitTest;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello Wrld");
         System.out.println("kasper");
 
     }
